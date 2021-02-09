e1fbf6676626318db085c0d56c41a2815fbe8eb9
KonstantinosGer <kgerasovits@gmail.com>
2021-02-09 19:45:10 +0200

    Models Commit

---
+++ app/src/main/java/com/unipi/p17019p17024/clickawayapplication/Accessories.java
@@ -0,0 +1,55 @@
+package com.unipi.p17019p17024.clickawayapplication;
+
+import java.io.File;
+
+public class Accessories {
+
+    private String id;
+    private String name;
+    private double price;
+    private int quantity;
+    private File image;
+
+    public Accessories() {}
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public File getImage() {
+        return image;
+    }
+
+    public void setImage(File image) {
+        this.image = image;
+    }
+
+}
---
+++ app/src/main/java/com/unipi/p17019p17024/clickawayapplication/Coffee.java
@@ -0,0 +1,64 @@
+package com.unipi.p17019p17024.clickawayapplication;
+
+import java.io.File;
+
+public class Coffee {
+
+    private String id;
+    private String name;
+    private double price;
+    private int quantity;
+    private String type;
+    //private Image image;
+    private File image;
+
+    public Coffee() {}
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public File getImage() {
+        return image;
+    }
+
+    public void setImage(File image) {
+        this.image = image;
+    }
+}
