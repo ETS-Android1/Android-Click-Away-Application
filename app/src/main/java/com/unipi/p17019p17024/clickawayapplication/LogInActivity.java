e725cea18e21320d0e9a54d89fb8115104927064
KonstantinosGer <kgerasovits@gmail.com>
2021-02-22 01:56:09 +0200

    SP2 Commit Ger

--- app/src/main/java/com/unipi/p17019p17024/clickawayapplication/LogInActivity.java
+++ app/src/main/java/com/unipi/p17019p17024/clickawayapplication/LogInActivity.java
@@ -18,7 +18,7 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class LogInActivity extends AppCompatActivity {
-    EditText editTextEmail,editTextPassword,editTextTextUsername;
+    EditText editTextEmail, editTextPassword, editTextTextUsername;
     Button button1,button2,button3,button4;
     TextView textView1,textView2;
     CheckBox checkBox, checkBox2;
@@ -31,7 +31,6 @@ public class LogInActivity extends AppCompatActivity {
     FirebaseUser currentUser;
 
 
-    //boolean isSignedIn = false;
     boolean isSignInPushed = false;
 
 
@@ -56,10 +55,8 @@ public class LogInActivity extends AppCompatActivity {
         //
         preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         //Email
-        //String str_email = preferences.getString("myKeyEmail", "");
         editTextEmail.setText("");
         //Password
-        //String str_password = preferences.getString("myKeyPassword", "");
         editTextPassword.setText("");
 
         checkBox.setChecked(false);
@@ -119,9 +116,6 @@ public class LogInActivity extends AppCompatActivity {
         checkBox.setVisibility(View.INVISIBLE);
         checkBox2.setVisibility(View.VISIBLE);
 
-        //checkBox.setPadding(130, 390, 131, 320);
-
-        //writeSP(view);
 
         String str_email = preferences.getString("myKeyEmail", "");
         editTextEmail.setText(str_email);
@@ -129,11 +123,9 @@ public class LogInActivity extends AppCompatActivity {
         editTextPassword.setText(str_password);
 
         if(str_email == "" && str_password == ""){
-            //checkBox.setChecked(false);
             checkBox2.setChecked(false);
         }
         else {
-            //checkBox.setChecked(true);
             checkBox2.setChecked(true);
         }
 
@@ -152,9 +144,7 @@ public class LogInActivity extends AppCompatActivity {
         checkBox.setVisibility(View.VISIBLE);
         checkBox2.setVisibility(View.INVISIBLE);
 
-        //String str_email = preferences.getString("myKeyEmail", "");
         editTextEmail.setText("");
-        //String str_password = preferences.getString("myKeyPassword", "");
         editTextPassword.setText("");
 
         checkBox.setChecked(false);
@@ -170,7 +160,7 @@ public class LogInActivity extends AppCompatActivity {
                             Toast.makeText(getApplicationContext(), "Log-in successful!", Toast.LENGTH_LONG).show();
 
                             isSignInPushed = true;
-                            writeSP(isSignInPushed); //writeSP(view, isSignInPushed);
+                            writeSP(isSignInPushed);
 
                             Intent intent2 = new Intent(getApplicationContext(), MainActivity.class);
                             intent2.putExtra("userID", currentUser.getUid());
@@ -192,34 +182,8 @@ public class LogInActivity extends AppCompatActivity {
                 .addOnCompleteListener(task -> Toast.makeText(getApplicationContext(),"User created",Toast.LENGTH_LONG).show());
     }
 
-    public void writeSP(Boolean isSignInPushed) {  //View view,
+    public void writeSP(Boolean isSignInPushed) {
         SharedPreferences.Editor editor = preferences.edit();
-
-        /*if(checkBox.isChecked() && checkBox2.isChecked()) {
-            //Email
-            editor.putString("myKeyEmail", editTextEmail.getText().toString());
-            editor.apply();
-            //Password
-            editor.putString("myKeyPassword", editTextPassword.getText().toString());
-            editor.apply();
-        }
-        else if(checkBox.isChecked() && !checkBox2.isChecked()){
-            if (!isSignInPushed){
-
-            }
-        }
-        else if(!checkBox.isChecked() && checkBox2.isChecked()){
-
-        }
-        else{
-            //Email
-            editor.putString("myKeyEmail", "");
-            editor.apply();
-            //Password
-            editor.putString("myKeyPassword", "");
-            editor.apply();
-        }*/
-
 
         if (isSignInPushed){
             if(checkBox2.isChecked()) {
